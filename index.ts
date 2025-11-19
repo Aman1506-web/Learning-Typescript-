5188dbbcd370c166dfe871515a249498fb4c2fc9
Aman Sharma <sharmaa1518@gmail.com>
2025-11-19 12:47:35 +0530

    Learning TS

---
+++ index.ts
@@ -0,0 +1,56 @@
+
+type PizzaObj = {
+  name: string
+  price: number
+}
+
+const menu = [
+  { name: "Margherita", price: 8 },
+  { name: "Pepproni", price: 10 },
+  { name: "Hawaiian", price: 10 },
+  { name: "Veggie", price: 9 },
+];
+
+let cashInRegister = 100;
+const orderQueue = [];
+let nextOrderId = 1;
+
+function addNewPizza(pizzaObj: PizzaObj) {
+  menu.push(pizzaObj);
+}
+
+function placeOrder(pizzaName) {
+  const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
+  if(selectedPizza === undefined){
+    console.error(`${pizzaName} doesn't exist in menu`)
+  }
+
+  cashInRegister += selectedPizza?.price;
+
+  const newOrder = {
+    id: nextOrderId++,
+    pizza: selectedPizza,
+    status: "ordered",
+  };
+  orderQueue.push(newOrder);
+
+  return newOrder;
+}
+  
+function completeOrder(orderId: number) {
+  const order = orderQueue.find((order) => order.id === orderId);
+  order.status = "completed";
+  return order;
+}
+
+addNewPizza({ pizza: "Chicken Bacon", price: 12 });
+addNewPizza({ pizza: "BBQ Chicken", price: 11 });
+addNewPizza({ pizza: "Spicy Sausage", price: 11 });
+
+placeOrder("Chicken Bacon")
+completeOrder(1)
+
+console.log(menu)
+console.log(`cashInRegister is ${cashInRegister}`)
+console.log(`Order queue is ${orderQueue}`)
+console.log(`next order id is ${nextOrderId}`)
---
+++ index1.ts
@@ -0,0 +1,22 @@
+
+// custom types
+type Food = string;
+let favoruiteFood: Food = "Pizza"
+
+type Person = {
+  name: string
+  age: number
+  isStudent: boolean
+}
+
+let person1: Person = {
+  name: "Joe",
+  age: 42,
+  isStudent : true,
+}
+
+let person2: Person = {
+  name: "Jilly",
+  age: 34,
+  isStudent: false,
+}
